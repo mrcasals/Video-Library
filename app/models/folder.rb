5df78bc9c7a940e50b2f5e42a9ef5390e7882915
Marc Riera Casals <mrc2407@gmail.com>
2011-03-22 19:59:07 +0100

    Get parent folder accessible

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -1,6 +1,7 @@
 class Folder < ActiveRecord::Base
 
   has_many :folders, foreign_key: 'parent_id', :class_name => 'Folder', dependent: :destroy
+  belongs_to :parent, :class_name => 'Folder'
   has_many :videos
 
   default_scope order(:name)
