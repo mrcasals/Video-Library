6a5c6657a21351d5200bacd5a81c6264fea2f3ec
mrc2407 <mrc2407@gmail.com>
2011-03-25 19:36:24 +0100

    Create seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+User.create(:username => "admin", :password => "test123", :password_confirmation => "test123", :admin => true, :email => "admin@lvh.me")
+
+Folder.create([{:name => "Films"}, {:name => "Documentals"}])
+
+Video.create([{:name => "Video 1", :link => "blah", :folder_id => 1}, {:name => "Video 2", :link => "blah", :folder_id => 2}])
