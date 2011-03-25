91a0dc7e9dd1a26e47fa0eee1cbd6245209dd102
Marc Riera Casals <mrc2407@gmail.com>
2011-03-25 16:26:20 +0100

    Starting admin section

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,6 +1,8 @@
 class Admin::ApplicationController < ::ApplicationController
   before_filter :authenticate_user!
 
+  layout 'private'
+
   def authenticate_admin!
     current_user.admin?
   end
--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -1,6 +1,3 @@
 class Admin::DashboardsController < Admin::ApplicationController
 
-  inherit_resources 
-  actions :show
-
 end
