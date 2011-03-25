479be2a70383dcc9abae7a2dca962d7ac90ffd64
Marc Riera Casals <mrc2407@gmail.com>
2011-03-25 13:22:42 +0100

    Let users login with username instead of email

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110325114118) do
+ActiveRecord::Schema.define(:version => 20110325120329) do
 
   create_table "folders", :force => true do |t|
     t.string  "name"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(:version => 20110325114118) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "admin",                               :default => false
+    t.string   "username"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
