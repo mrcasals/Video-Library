1e9b87806b147b1502ed02657b693fb15c3d7cfc
Marc Riera Casals <mrc2407@gmail.com>
2011-03-25 12:44:09 +0100

    Add admin role to users

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,3 +1,7 @@
 class Admin::ApplicationController < ::ApplicationController
-    before_filter :authenticate_user!
+  before_filter :authenticate_user!
+
+  def authenticate_admin!
+    current_user.admin?
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110322185716) do
+ActiveRecord::Schema.define(:version => 20110325114118) do
 
   create_table "folders", :force => true do |t|
     t.string  "name"
@@ -18,9 +18,9 @@ ActiveRecord::Schema.define(:version => 20110322185716) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",                               :default => "", :null => false
-    t.string   "encrypted_password",   :limit => 128, :default => "", :null => false
-    t.string   "password_salt",                       :default => "", :null => false
+    t.string   "email",                               :default => "",    :null => false
+    t.string   "encrypted_password",   :limit => 128, :default => "",    :null => false
+    t.string   "password_salt",                       :default => "",    :null => false
     t.string   "reset_password_token"
     t.string   "remember_token"
     t.datetime "remember_created_at"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(:version => 20110322185716) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "admin",                               :default => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
