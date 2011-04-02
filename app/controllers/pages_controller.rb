6fd7ad7b0bd8db3267e74a49d2b5341ddd58f789
Marc Riera Casals <mrc2407@gmail.com>
2011-04-02 17:57:50 +0200

    Upload views per page on homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def home
-    @videos = Video.order_by_desc(:created_at).page(params[:page]).per(2)
+    @videos = Video.order_by_desc(:created_at).page(params[:page]).per(5)
 
   end
 end
