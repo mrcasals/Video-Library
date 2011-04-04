97ccaf854a4a1ec1cdf5d74dd08a18b3bef6b455
Marc Riera Casals <mrc2407@gmail.com>
2011-04-04 15:10:12 +0200

    Remove ugly <br /> from devise views

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -10,4 +10,18 @@ class Folder < ActiveRecord::Base
 
   validates :name, presence: true
 
+  def self.name_with_tree
+    array = Array.new
+    Folder.all.each do |folder|
+      if !folder.parent_id.blank?
+        folder_name = "#{folder.parent.name} -> #{folder.name}"
+        folder_tree = [ folder_name, folder.id ]
+      else
+        folder_tree = [ folder.name, folder.id ]
+      end
+      array << folder_tree
+    end
+    return array
+  end
+
 end
