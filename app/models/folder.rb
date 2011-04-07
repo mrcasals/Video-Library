9779ff9019e4e1dc6112b114548528c5cb55773c
Marc Riera Casals <mrc2407@gmail.com>
2011-04-07 18:01:38 +0200

    Update :dependent => :destroy

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -1,8 +1,8 @@
 class Folder < ActiveRecord::Base
 
-  has_many :folders, foreign_key: 'parent_id', :class_name => 'Folder', dependent: :destroy
+  has_many :folders, foreign_key: 'parent_id', :class_name => 'Folder', :dependent => :destroy
   belongs_to :parent, :class_name => 'Folder'
-  has_many :videos
+  has_many :videos, :dependent => :destroy
 
   has_friendly_id :name, :use_slug => true
 
